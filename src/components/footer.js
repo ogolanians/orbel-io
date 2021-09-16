21b45d3b37cd29e3c03dc10018865e622c0170d7
Orbel Golanians <ogolanians@gmail.com>
2021-09-15 18:11:05 -0700

    Added basic page and component files

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Created by Orbel Golanians, Copyright 2021</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
