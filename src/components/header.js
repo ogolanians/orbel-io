db2b1b27fb669b99be69bb2aa9f544ca62debb2f
Orbel Golanians <ogolanians@gmail.com>
2021-12-12 22:31:03 -0800

    Nav menu responsiveness improvement

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "gatsby";
 import OrbelLogo from "../assets/orbel-logo-2.png";
 import { RiMenu3Line } from "react-icons/ri";
+import { RiCloseFill } from "react-icons/ri";
 
 import * as headerStyles from "./header.module.scss";
 
 const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => {
+    console.log("hellooo");
+    setSidebar(!sidebar);
+  };
+
   return (
     <header className={headerStyles.header}>
       <Link to="/">
@@ -15,58 +24,67 @@ const Header = () => {
           alt="Orbel Golanians' logo"
         ></img>
       </Link>
-      <div className={headerStyles.navMenu}>
-        <RiMenu3Line />
+      <div className={headerStyles.navMenu} onClick={showSidebar}>
+        {sidebar ? (
+          <ul>
+            <RiCloseFill />
+            <nav>
+              <ul className={headerStyles.navList}>
+                <li>
+                  <Link
+                    className={headerStyles.navItem}
+                    activeClassName={headerStyles.activeNavItem}
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={headerStyles.navItem}
+                    activeClassName={headerStyles.activeNavItem}
+                    to="/about"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={headerStyles.navItem}
+                    activeClassName={headerStyles.activeNavItem}
+                    to="/projects"
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={headerStyles.navItem}
+                    activeClassName={headerStyles.activeNavItem}
+                    to="/blog"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={headerStyles.navItem}
+                    activeClassName={headerStyles.activeNavItem}
+                    to="/contact"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </ul>
+        ) : (
+          <nav>
+            <RiMenu3Line />
+          </nav>
+        )}
+        {/* <RiMenu3Line onClick={showSidebar} /> */}
       </div>
-      <nav>
-        <ul className={headerStyles.navList}>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/projects"
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/blog"
-            >
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/contact"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
